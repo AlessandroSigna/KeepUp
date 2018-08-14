4c1311ad0b2671ce63bc7c50aa9265372868ddde
AlessandroSigna <alesigna92@gmail.com>
2018-08-14 15:02:10 +0200

    basic game mechanics

---
+++ Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour {
+
+    public GameObject blockPrefab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            var mousePos = Input.mousePosition;
+            var objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+            objectPos.z = 0;
+            Instantiate(blockPrefab, objectPos, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/CharacterBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBehaviour : MonoBehaviour {
+
+    public float moveForce = 0;
+    public float maxSpeed = 0;
+    public float jumpHeight = 0;
+    public Transform groundCheckPoint;
+    public LayerMask whatIsGround;
+    public float groundCheckRadius;
+    public bool isGrounded = false;
+    public float speed;
+
+    private Rigidbody2D _rigidbody;
+    private bool jumping = false;
+	// Use this for initialization
+	void Start () {
+        _rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+        if (isGrounded && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)))
+        {
+            //jump
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jumpHeight);
+        }
+        if (Input.GetKey(KeyCode.A) )
+        {
+            //left
+            _rigidbody.AddForce(new Vector2(-moveForce, 0), ForceMode2D.Force);
+        }
+        if (Input.GetKey(KeyCode.D) )
+        {
+            //right
+            _rigidbody.AddForce(new Vector2(moveForce, 0), ForceMode2D.Force);
+        }
+        speed = _rigidbody.velocity.x;
+        if (speed <= -maxSpeed)
+            _rigidbody.velocity = new Vector2( -maxSpeed, _rigidbody.velocity.y);
+        else if (speed >= maxSpeed)
+            _rigidbody.velocity = new Vector2(maxSpeed, _rigidbody.velocity.y);
+    }
+}
