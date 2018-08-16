90b4cd998a2d6db9b20c60ad7b2a5cd0e8192246
AlessandroSigna <alesigna92@gmail.com>
2018-08-16 16:03:04 +0200

    swim logic

---
+++ Assets/Scripts/WaterBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Phill")
+        {
+            GameOverManager.Instance.GameOver();
+        }
+
+        CharacterBehaviour character = collision.gameObject.GetComponent<CharacterBehaviour>();
+        if (character != null)
+        {
+            character.isSwimming = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        CharacterBehaviour character = collision.gameObject.GetComponent<CharacterBehaviour>();
+        if (character != null)
+        {
+            character.isSwimming = false;
+        }
+    }
+}
